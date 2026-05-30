4179af3d03a0bcfec509e4de450386510b7a5510
Ademola-giod <idowuademola65@gmail.com>
2026-05-30 02:03:18 +0100

    updated authmiddleware

--- press-backend/middleware/authMiddleware.js
+++ press-backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import { auth } from 'express-oauth2-jwt-bearer';
 import 'dotenv/config';
 import axios from 'axios';
 import User from '../models/User.js';
-import { ADMIN_EMAILS } from './whiteList.js';
+import { ADMIN_EMAILS } from '../middleware/whiteList.js';
 
 // 1. Verify Auth0 Token
 export const checkJwt = auth({
