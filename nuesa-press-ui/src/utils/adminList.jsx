96eaf0f91d7fc8c1fccc8e80bc1ede7b3eee74b0
Ademola-giod <idowuademola65@gmail.com>
2026-06-27 11:46:30 +0100

    saved admin mail

--- nuesa-press-ui/src/utils/adminList.jsx
+++ nuesa-press-ui/src/utils/adminList.jsx
@@ -2,5 +2,6 @@
 export const ADMIN_EMAILS = [
   // "idowuademola65@gmail.com", // Add your email here to test the Admin view!
   "aidowu864@stu.ui.edu.ng",
+  "yoyewo880@stu.ui.edu.ng",
   // ... add the rest later
 ];
